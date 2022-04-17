afedc4dcf433119fd7e2a8e9aeb5e77c543d6e25
william bingo <wbii@qq.com>
2022-04-17 18:02:32 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 96);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始备份";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(253, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "先将本程序放于升级dll文件夹下\r\n再填入需要备份的文件夹路径\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(723, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string SelectPath()
+
+        {
+
+            string targetFilePath = string.Empty;
+
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+
+            {
+
+                targetFilePath = fbd.SelectedPath;
+
+            }
+
+            return targetFilePath;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //选择路径改为输入路径
+            //string targetFilePath = SelectPath();
+            string targetFilePath = textBox1.Text;
+            string sourceFilePath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            String bakFilePath = sourceFilePath + "bak" + DateTime.Now.ToString("yyyyMMddhhmmss");
+
+            if (Directory.Exists(bakFilePath) == false)
+            {
+                Directory.CreateDirectory(bakFilePath);
+            }
+            //string bakFilePath = System.IO.Path.Combine(sourceFilePath, "bak");
+            CopyFilefolder(sourceFilePath, targetFilePath, bakFilePath);
+
+
+        }
+
+        public static void CopyFilefolder(string sourceFilePath, string targetFilePath, string bakFilePath)
+        {
+            //获取源文件夹中的所有非目录文件
+
+            string[] filenames1 = Directory.GetFiles(targetFilePath);
+            string[] filenames2 = Directory.GetFiles(sourceFilePath);
+            string fileName;
+            string destFile;
+            foreach (string filename1 in filenames1)
+
+            {
+
+                //遍历文件夹2中所有文件
+
+                foreach (string filename2 in filenames2)
+
+                {
+
+                    //获取带扩展的文件名
+
+                    string name1 = Path.GetFileName(filename1);
+
+                    string name2 = Path.GetFileName(filename2);
+                    fileName = Path.GetFileName(filename2);
+                    destFile = Path.Combine(bakFilePath, fileName);
+
+                    //判断文件名是否相同
+
+                    if (name1 == name2)
+
+                    {
+
+                        //如果是有老版本的文件则存入bak文件夹
+
+                        File.Copy(filename1, destFile, true);
+
+                    }
+
+                }
+
+            }              
+            //如果目标文件夹不存在，则新建目标文件夹
+            //if (!Directory.Exists(targetFilePath))
+            //{
+            //   Directory.CreateDirectory(targetFilePath);
+            // }
+            //将获取到的文件一个一个拷贝到目标文件夹中 
+        
+            //上面一段在MSDN上可以看到源码 
+    
+            //获取并存储源文件夹中的文件夹名
+            //string[] filefolders = Directory.GetFiles(sourceFilePath);
+            //创建Directoryinfo实例 
+            //DirectoryInfo dirinfo = new DirectoryInfo(sourceFilePath);
+            //获取得源文件夹下的所有子文件夹名
+           // DirectoryInfo[] subFileFolder = dirinfo.GetDirectories();
+           // for (int j = 0; j < subFileFolder.Length; j++)
+           // {
+                //获取所有子文件夹名 
+             //   string subSourcePath = sourceFilePath + "\\" + subFileFolder[j].ToString();
+              //  string subTargetPath = targetFilePath + "\\" + subFileFolder[j].ToString();
+                //把得到的子文件夹当成新的源文件夹，递归调用CopyFilefolder
+               // CopyFilefolder(subSourcePath, subTargetPath, bakFilePath);
+            //}
+    
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
